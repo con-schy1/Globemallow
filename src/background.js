e9d538061f7470c44617126cc93568feb237324a
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2025-12-08 20:57:13 +0530

    restructure

---
+++ src/background.js
@@ -0,0 +1,95 @@
+// changes made(global variable for sites)
+const MAXSITES = 12;
+
+chrome.runtime.onInstalled.addListener((details) => {
+  chrome.storage.session.clear();
+});
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.status == "complete") {
+    chrome.tabs.sendMessage(tabId, { start: true });
+  }
+});
+
+//////////////////////
+async function postData(url = "", data, contentType) {
+  if (contentType == "json") {
+    var datastringfy = JSON.stringify(data);
+    data = datastringfy.replace(/[\r\n]+/gm, "");
+    cType = "application/json";
+  } else {
+    cType = "application/x-www-form-urlencoded";
+  }
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    credentials: "include",
+    mode: "cors",
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      "Content-Type": cType,
+      "Access-Control-Allow-Origin": "*",
+    },
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: data, // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+////////////////////////
+
+chrome.runtime.onMessage.addListener(async (request, sender) => {
+  // changes made(only store 10 sites data) -harshit
+  let data = await chrome.storage.session.get(null),
+    storedAt = 99999999999999999999999,
+    toRemove = null;
+  if (Object.keys(data).length >= MAXSITES) {
+    for (z in data) {
+      if (data[z].storedAt < storedAt) {
+        storedAt = data[z].storedAt;
+        toRemove = z;
+      }
+    }
+
+    chrome.storage.session.remove(toRemove);
+  }
+
+  chrome.storage.session.set({ ["tab" + sender.tab.id]: request });
+  chrome.storage.session.get("tab" + sender.tab.id).then((dat) => {
+    if (request.finalScore >= 92) {
+      var colorString = "#32a852";
+    } else if (request.finalScore >= 78) {
+      var colorString = "#8ECA2E";
+    } else if (request.finalScore >= 67) {
+      var colorString = "#f4e03a";
+    } else if (request.finalScore >= 55) {
+      var colorString = "#F77616";
+    } else {
+      var colorString = "#ff0d21";
+    }
+
+    chrome.action.setBadgeBackgroundColor({
+      color: colorString,
+      tabId: sender.tab.id,
+    });
+    chrome.action.setBadgeText({
+      text: request.finalGrade,
+      tabId: sender.tab.id,
+    });
+  });
+});
+
+// Clear Cache
+chrome.tabs.onRemoved.addListener((tabId) => {
+  chrome.storage.session.get("tab" + tabId).then((data) => {
+    delete data["tab" + tabId];
+  });
+});
+
+chrome.runtime.onInstalled.addListener(function (object) {
+  let externalUrl = "https://ko-fi.com/globemallow#paypalModal";
+
+  if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    chrome.tabs.create({ url: externalUrl }, function (tab) {
+      //console.log("New tab launched with http://yoursite.com/");
+    });
+  }
+});
---
+++ src/options/myscripts.js
@@ -0,0 +1,235 @@
+// changes below -harshit
+const AVERAGE = 70;
+const MAX = AVERAGE * 2;
+
+const ctx = document.querySelector("#myChart").getContext("2d");
+
+let hosts = [];
+class Hosts {
+  constructor(time, url, Sustainability, Score) {
+    this.time = time;
+    this.url = url;
+    this.Score = Score;
+    this.Sustainability = Sustainability;
+  }
+}
+class Dataset {
+  constructor(label, data) {
+    this.base = AVERAGE;
+    this.label = label;
+    this.data = data; //hosts.map(z => z[label]);
+    this.backgroundColor = (context) => {
+      const index = context.dataIndex;
+      const value = context.dataset.data[index];
+      return value < this.base
+        ? "rgba(200, 0, 0, 0.6)"
+        : "rgba(0, 200, 0, 0.6)";
+    };
+    this.borderColor = this.backgroundColor;
+    this.borderWidth = 2;
+    this.borderRadius = 5;
+    this.borderSkipped = false;
+    this.fill = {
+      target: AVERAGE,
+      above: "green",
+      below: "red",
+    };
+  }
+}
+
+function chartConfig(storageData) {
+  hosts.length = 0;
+  for (z in storageData) {
+    if (!hosts.length) {
+      hosts.push(
+        new Hosts(
+          storageData[z].storedAt,
+          storageData[z].hostURL,
+          storageData[z].Score,
+          storageData[z].Sustainability,
+        ),
+      );
+
+      continue;
+    }
+
+    let added = false;
+    for (let i = 0; i < hosts.length; i++) {
+      let y = hosts[i];
+      if (storageData[z].storedAt < y.time) {
+        let firstHalf = hosts.splice(0, i);
+        firstHalf.push(
+          new Hosts(
+            storageData[z].storedAt,
+            storageData[z].hostURL,
+            storageData[z].Score,
+            storageData[z].Sustainability,
+          ),
+        );
+        hosts = firstHalf.concat(hosts);
+
+        added = true;
+        break;
+      }
+    }
+
+    if (!added) {
+      hosts.push(
+        new Hosts(
+          storageData[z].storedAt,
+          storageData[z].hostURL,
+          storageData[z].Score,
+          storageData[z].Sustainability,
+        ),
+      );
+    }
+  }
+
+  const labels = ["Sustainability", "Score"];
+  const dataTotal = [];
+  hosts.forEach((z) => {
+    let temp = 0;
+    let tempLabel = "";
+    labels.forEach((y) => {
+      temp += z[y];
+    });
+    dataTotal.push(temp);
+  });
+
+  const data = {
+    labels: hosts.map((z) => z.url),
+    datasets: [new Dataset(labels.join(" "), dataTotal)],
+  };
+
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+          position: "top",
+        },
+        title: {
+          display: false,
+          text: "Chart.js Bar Chart",
+        },
+      },
+      scales: {
+        x: {},
+        y: {
+          min: 30,
+          max: 100,
+          ticks: {
+            stepSize: 5,
+          },
+        },
+      },
+    },
+  };
+
+  return config;
+}
+
+function createChart(callback, storageData) {
+  if (!callback) return 0;
+
+  return new Chart(ctx, callback(storageData));
+}
+function updateChartData(x) {
+  //chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data = [x.totalTot, x.totalDiff];
+  });
+  chart.update();
+}
+
+let chart = null;
+chrome.storage.session.get(null).then((data) => {
+  chart = createChart(chartConfig, data);
+});
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  chrome.storage.session.get(null).then((data) => {
+    chart.destroy();
+    chart = createChart(chartConfig, data);
+
+    if (window.location.hash == "#Requests") {
+      requestDiv.innerHTML = "";
+      for (x in data) {
+        listSiteInfo(
+          data[x].hostURL,
+          data[x].imgNotLLArray,
+          data[x].imgNotGoodFormat,
+          data[x].imgNotRes,
+        );
+      }
+    }
+  });
+});
+
+// changes below -harshit
+// ----------------- iframe and image list DOM -----------------
+const requestDiv = document.querySelector("#requestDiv");
+const siteInfoTemplate = document.querySelector("#site-info-template");
+
+function listSiteInfo(name, notLL, format, notResp) {
+  let clone = siteInfoTemplate.content.cloneNode(true);
+
+  let siteName = clone.querySelector(".site-name");
+  let siteMore = clone.querySelector(".site-more");
+  let siteImageList = clone.querySelector(".site-image-list");
+  let siteFormatList = clone.querySelector(".site-format-list");
+  let siteRespList = clone.querySelector(".site-responsive-list");
+  let listsections = clone.querySelectorAll(".list-sections");
+
+  siteName.innerText = name;
+  siteMore.addEventListener("click", (e) => {
+    listsections.forEach((z) => {
+      z.classList.toggle("list-sections");
+    });
+    e.currentTarget.classList.toggle("site-more-rotated");
+  });
+
+  //Connor Try / Catch
+  try {
+    notLL.forEach((z) => {
+      if (z && z != "") siteImageList.innerHTML += `<li>${z}</li>`;
+    });
+
+    format.forEach((z) => {
+      if (z && z != "") siteFormatList.innerHTML += `<li>${z}</li>`;
+    });
+
+    notResp.forEach((z) => {
+      if (z && z != "") siteRespList.innerHTML += `<li>${z}</li>`;
+    });
+
+    requestDiv.appendChild(clone);
+  } catch (e) {}
+}
+
+//Chart Code to display requestDiv and hide chartDiv
+window.addEventListener("hashchange", async function () {
+  if (location.hash === "#Requests") {
+    document.getElementById("chartDiv").style.display = "none";
+    document.getElementById("requestDiv").style.display = "block";
+
+    // changes below -harshit
+    requestDiv.innerHTML = "";
+    await chrome.storage.session.get(null).then((data) => {
+      for (x in data) {
+        listSiteInfo(
+          data[x].hostURL,
+          data[x].imgNotLLArray,
+          data[x].imgNotGoodFormat,
+          data[x].imgNotRes,
+        );
+      }
+    });
+  } else {
+    document.getElementById("chartDiv").style.display = "block";
+    document.getElementById("requestDiv").style.display = "none";
+  }
+});
